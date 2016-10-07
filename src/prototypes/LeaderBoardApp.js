72b0f90bbd6fbf69de416e5ef836cbeb1ef0573e
Ziying Zhang <zzhang@coursera.org>
2016-10-06 21:09:18 -0700

    Initial comit

--- src/prototypes/LeaderBoardApp.js
+++ src/prototypes/LeaderBoardApp.js
@@ -1,89 +1,382 @@
 const React = require('react');
+const classNames = require('classnames');
 const _ = require('underscore');
+const courseraLeaderboard = require('./courseraLeaderboard');
+
+const courseCache = {
+  learn: {
+    courseName: 'Learning How to Learn',
+    school: 'University of Califronia, San Diego',
+    photoUrl: 'https://s3.amazonaws.com/coursera-university-assets/leaderboard/thumb_howtolearn_80.png',
+  },
+  machineLearning: {
+    courseName: 'Machine Learning',
+    school: 'Stanford University',
+    photoUrl: 'https://s3.amazonaws.com/coursera-university-assets/leaderboard/thumb_machine_learning_80.png',
+  },
+  dog: {
+    courseName: 'Dog Emotion and Cognition',
+    school: 'Duke University',
+    photoUrl: 'https://s3.amazonaws.com/coursera-university-assets/leaderboard/thumb_dog_80.png',
+  },
+};
 const cardStyles = {
   card: {
-    paddingTop: 16,
-    minWidth: 600,
-    marginBottom: 32,
+    minWidth: 706,
+    minHeight: 196,
+    padding: 32,
+    borderBottom: '1px solid #c4c4c4'
   },
   mainCardContentBox: {
     marginRight: 32,
+    marginLeft: 45,
   },
   numberBox: {
     minWidth: 80,
+    marginTop: 20,
+    position: "relative",
   },
   avatar: {
     borderRadius: '50%',
-    width: 48,
-    height: 48,
-    marginRight: 16,
+    width: 140,
+    height: 140,
+  },
+  avatarcontainer: {
+    position: 'relative',
   },
   pointsBox: {
     minWidth: 80,
+    textAlign: "left",
   },
+  pointsTitle: {
+    color: "#969696",
+    fontSize: 12,
+    fontFace: 'OpenSans-SemiBold',
+  },
+  pointsNumber: {
+    color: "#48CFAD",
+    fontSize: 64,
+    fontFace: 'OpenSans',
+    character: '-0.5px',
+    line: '87px'
+  },
+  numberCircle: {
+    float: 'right',
+    width: 55,
+    height: 55,
+    borderRadius: '50%',
+    textAlign: "center",
+    backgroundColor: "#48CFAD",
+    color: "white",
+    fontSize: 20,
+    lineHeight: '280%',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+  mainCardName: {
+    fontSize: 24,
+    fontFace: 'OpenSans-SemiBold',
+    marginBottom: 8,
+  },
+  mainCardCompleted: {
+    fontSize: 12,
+    color: "#969696",
+    fontFace: 'OpenSans-SemiBold',
+  },
+  mainCardCurrent: {
+    fontSize: 12,
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  mainCardCurrentTitle: {
+    fontSize: 18,
+  },
+  mainCardCurrentSchool: {
+    fontSize: 12,
+    color: "#c4c4c4",
+  },
+  coursePicture: {
+    width: 80,
+    height: 80,
+    marginRight: 16,
+  },
+  progressBar: {
+    width: 240,
+    height: 11,
+    display: 'block',
+    borderRadius: 6,
+  },
+  progressStyle: {
+    width: '50%',
+    height: 12,
+    borderRadius: 6,
+    margin: '8px 0',
+  }
 };
 
-const Card = ({rank = 1, name = 'Card'}) => {
+class ProgressBarWidget extends React.Component {
+  // static propTypes = {
+  //   isDanger: React.PropTypes.bool,
+  //   percentComplete: React.PropTypes.number
+  // }
+
+  // static defaultProps = {
+  //   isDanger: false,
+  //   percentComplete: 0,
+  // }
+
+  render() {
+    const {isDanger, percentComplete} = this.props;
+    let percentCompleteLocal = percentComplete;
+    if (percentComplete < 0) {
+      percentCompleteLocal = 0;
+    } else if (percentComplete > 100) {
+      percentCompleteLocal = 100;
+    }
+    const progressClasses = classNames(
+        'progress-completed',
+        {'bgcolor-success': !isDanger},
+        {'bgcolor-danger': isDanger},
+      );
+    const progressStyles = {
+      width: percentCompleteLocal + '%'
+    };
+
+    return (
+      <div className="rc-ProgressBarWidget bgcolor-black-g1">
+        <div className={progressClasses} style={cardStyles.progressStyle}></div>
+      </div>
+    );
+  }
+}
+
+ProgressBarWidget.defaultProps = {
+    isDanger: false,
+    percentComplete: 0,
+}
+ProgressBarWidget.propTypes = {
+    isDanger: React.PropTypes.bool,
+    percentComplete: React.PropTypes.number
+}
+
+const Card = ({data}) => {
+  const percent = 10;
+  data.photoUrl = ('0' + data.rank).slice(-2) + '_' + data.userName.toLowerCase().replace(' ', '_') + '.png';
+
   return (
       <div className="rc-Card horizontal-box" style={cardStyles.card}>
         <div className="number-box horizontal-box align-items-absolute-center" style={cardStyles.numberBox}>
-          <h2 className="headline-text-2">{rank}</h2>
-        </div>
-        <div className="main-card-content-box horizontal-box flex-1" style={cardStyles.mainCardContentBox}>
-          <div className="avatar-container">
-            <img src="//placehold.it/100x100/A66506/FFFFFF" alt="CourseraAlt" style={cardStyles.avatar} />
+          <div clasesName="number-circle" style={cardStyles.numberCircle}>
+            {data.rank}
           </div>
-          <div className="text-content-container">
-            <h2 className="headline-text-3">Lorem ipsum dolor sit.</h2>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam error ratione et dolor laborum reprehenderit at odio explicabo optio architecto.
-          </div>
-          <div className="points-box vertical-box align-items-absolute-center" style={cardStyles.pointsBox}>
-            <h2 className="headline-text-1 color-primary">{343}</h2>
-            <p className="label-text">Points</p>
+          <div className="avatar-container" style={cardStyles.avatarContainer}>
+            <img src={'https://s3.amazonaws.com/coursera-university-assets/leaderboard/' + data.photoUrl} alt="CourseraAlt" style={cardStyles.avatar} />
           </div>
         </div>
+        <div className="main-card-content-box vertical-box-box flex-1" style={cardStyles.mainCardContentBox}>
+          <div className="text-content-container main-card-top">
+            <div className="main-card-name" style={cardStyles.mainCardName}>{data.userName}</div>
+            <div className="main-card-courses-completed" style={cardStyles.mainCardCompleted}>{data.numCoursesCompleted} COURSES COMPLETED</div>
+            <div className="main-card-current-course" style={cardStyles.mainCardCurrent}>CURRENT COURSE</div>
+          </div>
+          <div className="text-content-container main-card-bottom horizontal-box">
+            <div className="course-picture">
+              <img src={courseCache[data.course].photoUrl} alt="CourseraAlt" style={cardStyles.coursePicture} />
+            </div>
+            <div className="course-description">
+              <div className="main-card-current-course-title" style={cardStyles.mainCardCurrentTitle}>{courseCache[data.course].courseName}</div>
+              <div className="main-card-current-course-school" style={cardStyles.mainCardCurrentSchool}>{courseCache[data.course].school}</div>
+              <div className="main-card-current-course-title label-text color-black-g2">PROGRESS</div>
+              <ProgressBarWidget
+                style={cardStyles.progressBar}
+                percentComplete={50}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="points-box vertical-box" style={cardStyles.pointsBox}>
+            <p className="points-title" style={cardStyles.pointsTitle}>Points</p>
+            <div className="points-number" style={cardStyles.pointsNumber}>{data.score}</div>
+          </div>
       </div>
     );
 };
 
 const appStyles = {
   main: {
+    marginTop: -50
+  },
+  title: {
+    display: 'block',
+    color: '#FFFFFF',
+    fontSize: 64,
+    fontFamily: "OpenSans-Light",
+    lineHeight: 87
+  },
+  timeExpiry: {
+    display: 'block',
+    fontSize: 20,
+    fontFamily: 'OpenSans',
+    lineHeight: 27,
+    color: 'white'
   },
   headerContainer: {
-    minHeight: 200,
+    background: 'linear-gradient(to bottom right, #4FC1E9, #4A89DC)',
+    height: 500,
   },
   leftCardContainer: {
+    marginLeft:100,
     marginRight: 32,
   },
   profileContainer: {
-    width: 300,
+    width: 400,
+    marginRight: 100
+  },
+  courseContainer: {
+    width: 400,
+    marginRight: 32,
+    marginTop: 40,
   },
   textSpacing: {
     padding: 16,
+  },
+  selectedOverall: {
+    fontWeight: 800,
+    borderBottom: '2px solid #FFFFFF',
+    lineHeight: 25,
+  },
+  thisMonth: {
+    marginLeft: 60,
+    lineHeight: 25,
+  },
+  nav: {
+    color: 'white',
+    fontSize: 18,
+    fontFamily: 'OpenSans',
+    height: 60,
+    marginTop:-50,
+  },
+  courseSpacing: {
+    marginTop: 20
   }
 };
 
 class LeaderBoardApp extends React.Component {
   render() {
-    const arr = _.range(10);
+    const leaderboardData = [
+      {
+        "userName": "April Pascua",
+        "score": 381,
+        "numCoursesCompleted": 2,
+        "course": "learn",
+        "rank": 1
+      },
+      {
+        "userName": "Jerry Charumilind",
+        "score": 343,
+        "numCoursesCompleted": 2,
+        "course": "machineLearning",
+        "rank": 2
+      },
+      {
+        "userName": "Audrey Li",
+        "score": 315,
+        "numCoursesCompleted": 2,
+        "course": "machineLearning",
+        "rank": 3
+      },
+      {
+        "userName": "Giovanni Dubois",
+        "score": 304,
+        "numCoursesCompleted": 2,
+        "course": "machineLearning",
+        "rank": 4
+      },
+      {
+        "userName": "Brandon Tram",
+        "score": 295,
+        "numCoursesCompleted": 2,
+        "course": "machineLearning",
+        "rank": 5
+      },
+      {
+        "userName": "Zhaojun Zhang",
+        "score": 280,
+        "numCoursesCompleted": 2,
+        "course": "dog",
+        "rank": 6
+      },
+      {
+        "userName": "Kara Behnke",
+        "score": 274,
+        "numCoursesCompleted": 1,
+        "course": "learn",
+        "rank": 7
+      },
+      {
+        "userName": "Emily Sands",
+        "score": 251,
+        "numCoursesCompleted": 1,
+        "course": "machineLearning",
+        "rank": 8
+      },
+      {
+        "userName": "Ian Stuart",
+        "score": 233,
+        "numCoursesCompleted": 1,
+        "course": "machineLearning",
+        "rank": 9
+      },
+      {
+        "userName": "Kurt Apen",
+        "score": 229,
+        "numCoursesCompleted": 1,
+        "course": "dog",
+        "rank": 10
+      }
+    ];
 
+    const topCourses = ['learn', 'machineLearning', 'dog'];
     return (
       <div className="rc-LeaderBoardApp">
-        <div className="header-container horizontal-box align-items-absolute-center" style={appStyles.headerContainer}>
-          <h1 className="display-text-1">LeaderBoard</h1>
+        <div className="vertical-box align-items-absolute-center" style={appStyles.headerContainer}>
+            <h1 className="display-text-1" style={{color: 'white', fontSize: 60, fontWeight: 'normal', marginBottom: 48}}>Coursera Leaderboard</h1>
+            <h2 className="headline-text-4" style={{color: 'white'}}>21 DAYS LEFT</h2>
         </div>
+
         <div className="main horizontal-box" style={appStyles.main}>
-          <div className="left-card-container card-no-action flex-1" style={appStyles.leftCardContainer}>
-            {arr.map((item, index) =>
-              <Card key={index} rank={item} />
+          <div className="left-card-container card-no-action flex-2" style={appStyles.leftCardContainer}>
+            <div className="horizontal-box" style={{marginTop: -64}}>
+              <div className="horizontal-box" style={{marginRight: 64}}>
+                <h2 className="headline-text-4" style={{color: '#fff', borderBottom: '2px solid #fff', paddingBottom: 16}}>Overall Progress</h2>
+              </div>
+              <div className="horizontal-box">
+                <h2 className="headline-text-4" style={{color: '#fff', paddingBottom: 16, fontWeight: 'normal'}}>This Month</h2>
+              </div>
+            </div>
+            {leaderboardData.map((item, index) =>
+              <Card key={index} data={item} />
             )}
           </div>
           <div className="right-card-container">
-            <div className="card-no-action vertical-box" style={appStyles.profileContainer}>
-              <img src="//placehold.it/300x200/A66506/FFFFFF" alt="CourseraAlt" />
+            <img src="https://s3.amazonaws.com/coursera-university-assets/leaderboard/profile.png"/>
+            <div className="card-no-action vertical-box" style={appStyles.courseContainer}>
               <div style={appStyles.textSpacing}>
-                <h2 className="headline-text-3">Lorem ipsum dolor sit.</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur aequuntur natus earum eos maiores sit temporerit!</p>
+                <h2 className="headline-text-3">Top Courses</h2>
+                {topCourses.map(course => (
+                  <div className="text-content-container main-card-bottom horizontal-box" style={appStyles.courseSpacing}>
+                    <div className="course-picture">
+                      <img src={courseCache[course].photoUrl} alt="CourseraAlt" style={cardStyles.coursePicture} />
+                    </div>
+                    <div className="course-description">
+                      <div className="main-card-current-course-title" style={cardStyles.mainCardCurrentTitle}>{courseCache[course].courseName}</div>
+                      <div className="main-card-current-course-school" style={cardStyles.mainCardCurrentSchool}>{courseCache[course].school}</div>
+                      <div className="main-card-current-course-title" style={cardStyles.mainCardCurrentSchool}>126 learners enrolled</div>
+                      
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
---
+++ src/prototypes/courseraLeaderboard.js
@@ -0,0 +1,537 @@
+export default {
+"programId": "8kveAyjsEea2MgrBjcQhKw",
+"leaderboardData": [
+  {
+    "userName": "Jon Holman",
+    "score": 63,
+    "numCoursesCompleted": 4,
+    "currentCourse": "Statistical Inference",
+    "rank": 1
+  },
+  {
+    "userName": "Wenhao Xia",
+    "score": 61,
+    "numCoursesCompleted": 4,
+    "currentCourse": "Statistical Inference",
+    "rank": 2
+  },
+  {
+    "userName": "Abhinav Batra",
+    "score": 60,
+    "numCoursesCompleted": 4,
+    "currentCourse": "Statistical Inference",
+    "rank": 3
+  },
+  {
+    "userName": "Jorge Alejandro",
+    "score": 51,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 4
+  },
+  {
+    "userName": "Jorge Alejandro",
+    "score": 51,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Statistical Inference",
+    "rank": 4
+  },
+  {
+    "userName": "Adnan Patel",
+    "score": 49,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 6
+  },
+  {
+    "userName": "Matt Docherty",
+    "score": 49,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 6
+  },
+  {
+    "userName": "Phil Drexler",
+    "score": 49,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 6
+  },
+  {
+    "userName": "Hitoshi Kawashima",
+    "score": 49,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 6
+  },
+  {
+    "userName": "Unnam Chowdary",
+    "score": 48,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 10
+  },
+  {
+    "userName": "Kushan Thakkar",
+    "score": 48,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 10
+  },
+  {
+    "userName": "Jake Bruemmer",
+    "score": 48,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 10
+  },
+  {
+    "userName": "Ollie",
+    "score": 46,
+    "numCoursesCompleted": 3,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 13
+  },
+  {
+    "userName": "Augustine Santillan",
+    "score": 36,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 14
+  },
+  {
+    "userName": "Augustine Santillan",
+    "score": 36,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 14
+  },
+  {
+    "userName": "Ankit Piparsania",
+    "score": 35,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 16
+  },
+  {
+    "userName": "Pramil Jain",
+    "score": 35,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 16
+  },
+  {
+    "userName": "Manasa Dintyala",
+    "score": 34,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 18
+  },
+  {
+    "userName": "Michael Auld",
+    "score": 34,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 18
+  },
+  {
+    "userName": "Loutfi Kuret",
+    "score": 33,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 20
+  },
+  {
+    "userName": "Gaurav A Singh",
+    "score": 33,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 20
+  },
+  {
+    "userName": "Priya Kumar",
+    "score": 32,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 22
+  },
+  {
+    "userName": "Sal Mun",
+    "score": 32,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 22
+  },
+  {
+    "userName": "Jodi Clontz",
+    "score": 32,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 22
+  },
+  {
+    "userName": "Richard Li",
+    "score": 32,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 22
+  },
+  {
+    "userName": "Richard Trent",
+    "score": 31,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 26
+  },
+  {
+    "userName": "Adam Rochford",
+    "score": 31,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 26
+  },
+  {
+    "userName": "Evan Luo",
+    "score": 31,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 26
+  },
+  {
+    "userName": "Prakhar Maini",
+    "score": 30,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 29
+  },
+  {
+    "userName": "Eric Pao",
+    "score": 24,
+    "numCoursesCompleted": 2,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 30
+  },
+  {
+    "userName": "Andrew Labunka",
+    "score": 20,
+    "numCoursesCompleted": 1,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 31
+  },
+  {
+    "userName": "Andrew Labunka",
+    "score": 20,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 31
+  },
+  {
+    "userName": "Miji Kim",
+    "score": 20,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 31
+  },
+  {
+    "userName": "Apurv Priyam",
+    "score": 19,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 34
+  },
+  {
+    "userName": "Jason Vanderlinden",
+    "score": 19,
+    "numCoursesCompleted": 1,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 34
+  },
+  {
+    "userName": "Jason Vanderlinden",
+    "score": 19,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 34
+  },
+  {
+    "userName": "Jordan Less",
+    "score": 18,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 37
+  },
+  {
+    "userName": "Kalyan Srinivas Ponnam",
+    "score": 18,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 37
+  },
+  {
+    "userName": "Rui Pan",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "Getting and Cleaning Data",
+    "rank": 39
+  },
+  {
+    "userName": "Rui Pan",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "Statistical Inference",
+    "rank": 39
+  },
+  {
+    "userName": "Rui Pan",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "Exploratory Data Analysis",
+    "rank": 39
+  },
+  {
+    "userName": "Rui Pan",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 39
+  },
+  {
+    "userName": "Marco Chan",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 39
+  },
+  {
+    "userName": "Priya Ashok Kumar Choudhary",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 39
+  },
+  {
+    "userName": "Mehul Singh",
+    "score": 17,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 39
+  },
+  {
+    "userName": "Sean Scobell",
+    "score": 16,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 46
+  },
+  {
+    "userName": "Veena Calambur",
+    "score": 16,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 46
+  },
+  {
+    "userName": "Apurva Swarnakar",
+    "score": 16,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 46
+  },
+  {
+    "userName": "James DiBitetto",
+    "score": 16,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 46
+  },
+  {
+    "userName": "Jamie Murphey",
+    "score": 16,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 46
+  },
+  {
+    "userName": "Amir Hashemi",
+    "score": 15,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 51
+  },
+  {
+    "userName": "Arkal Vikas Vittal Hegde",
+    "score": 15,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 51
+  },
+  {
+    "userName": "Raghav Goyal",
+    "score": 15,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 51
+  },
+  {
+    "userName": "Joe Donlan",
+    "score": 15,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 51
+  },
+  {
+    "userName": "William Quan",
+    "score": 14,
+    "numCoursesCompleted": 1,
+    "currentCourse": "R Programming",
+    "rank": 55
+  },
+  {
+    "userName": "Colin Taggart",
+    "score": 7,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 56
+  },
+  {
+    "userName": "Colin Taggart",
+    "score": 7,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 56
+  },
+  {
+    "userName": "Vickye Jain",
+    "score": 6,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 58
+  },
+  {
+    "userName": "Vickye Jain",
+    "score": 6,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 58
+  },
+  {
+    "userName": "Sinclair Kwak",
+    "score": 5,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "Regression Models",
+    "rank": 60
+  },
+  {
+    "userName": "Sinclair Kwak",
+    "score": 5,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 60
+  },
+  {
+    "userName": "Sinclair Kwak",
+    "score": 5,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 60
+  },
+  {
+    "userName": "Farah",
+    "score": 5,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 60
+  },
+  {
+    "userName": "Farah",
+    "score": 5,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 60
+  },
+  {
+    "userName": "Kaustubh Gururaj",
+    "score": 3,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 65
+  },
+  {
+    "userName": "Gregory Caiola",
+    "score": 3,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 65
+  },
+  {
+    "userName": "Rafael Fernandez",
+    "score": 2,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 67
+  },
+  {
+    "userName": "Sree Kumandan",
+    "score": 2,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 67
+  },
+  {
+    "userName": "Sree Kumandan",
+    "score": 2,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 67
+  },
+  {
+    "userName": "Bako Feizi",
+    "score": 2,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 67
+  },
+  {
+    "userName": "Neil McQuarrie",
+    "score": 1,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 71
+  },
+  {
+    "userName": "Piyush Shroff",
+    "score": 0,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 72
+  },
+  {
+    "userName": "Pranava Goundan",
+    "score": 0,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 72
+  },
+  {
+    "userName": "Abhishek Roy",
+    "score": 0,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 72
+  },
+  {
+    "userName": "Prakhar Maini",
+    "score": 0,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "R Programming",
+    "rank": 72
+  },
+  {
+    "userName": "Zainul Jafferji",
+    "score": 0,
+    "numCoursesCompleted": "[NULL]",
+    "currentCourse": "The Data Scientist’s Toolbox",
+    "rank": 72
+  }
+]
+};
